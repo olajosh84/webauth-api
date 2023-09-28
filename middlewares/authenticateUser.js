4ef88bc61433894237b6f9820a5a0ea782ce3451
olajeks <olumuyiwajames@gmail.com>
2023-09-28 12:18:45 +0100

    update

--- middlewares/authenticateUser.js
+++ middlewares/authenticateUser.js
@@ -3,7 +3,7 @@ import asyncHandler from "./asyncHandler.js";
 
 const authenticateUser = asyncHandler ( async (req, res, next) => {
     const token = req.cookies.token;
-    console.log(token);
+    console.log(`cookie is: ${req.headers.cookie}`);
     if(token){
         try {
             const decoded = await jwt.verify(token, process.env.JWT_SECRET);
